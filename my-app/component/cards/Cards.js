7e377834c98bf44162e707ab34ff6052a0e70431
Laraib007 <lrbkhan01@gmail.com>
2024-07-03 23:27:39 +0500

    loading products

--- my-app/component/cards/Cards.js
+++ my-app/component/cards/Cards.js
@@ -1,19 +1,24 @@
 
 export const getStaticProps = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/photos')
-  const data = await res.json()
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos")
+  const labi = await res.json()
+  console.log(res)
   return { props: { labi, } }
+
 }
 
 
-const   Cards=({labi})=> {
+const Cards = ({labi})=> {
         return (
           <div>
             {labi && labi.map((labiP) => {
-return(<h1 key={labiP.id}>{labiP.id} 234</h1>)
+              {console.log(labiP.id)}
+return(<h1 key={labiP.id}>{labiP.id} 234</h1>
+  
+)
 
 })}
-            {console.log(labi)}
+            
           </div>
         );
       }
