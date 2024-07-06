4cbf3e26d198f3c068fc28fca5e5fdb75e5cd12d
Laraib007 <lrbkhan01@gmail.com>
2024-07-06 14:30:19 +0500

    url

--- my-app/pages/[costumers]/[custumerId]/index.js
+++ my-app/pages/[costumers]/[custumerId]/index.js
@@ -1,6 +1,7 @@
 import {Db} from "@/db/Db";
 import Link from "next/link";
 import {useRouter} from "next/router"
+import dynamic from "next/dynamic";
 export default function (){
     const router = useRouter()
     const {CustomerId} = router.query;
@@ -10,9 +11,9 @@ export default function (){
     <div>
      <h1>Project Page</h1>
      { Db.map((userDet) => {
-        let urlLast
+        let urlLast = window.location.href()
        let last =  Db.filter((x)=> x.name === urlLast)
-         console.log(last)
+         console.log(urlLast)
   return <h1> <Link href={`/costumers/${userDet.name}/${userDet.project}`}>{userDet.project}</Link></h1>
  })}
     </div>
