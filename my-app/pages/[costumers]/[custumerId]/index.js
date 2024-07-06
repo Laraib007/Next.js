6b6adbb9a4f349c0e670df06589532c057f4ddad
Laraib007 <lrbkhan01@gmail.com>
2024-07-06 15:02:31 +0500

    not working

--- my-app/pages/[costumers]/[custumerId]/index.js
+++ my-app/pages/[costumers]/[custumerId]/index.js
@@ -1,21 +1,26 @@
 import {Db} from "@/db/Db";
 import Link from "next/link";
 import {useRouter} from "next/router"
-import dynamic from "next/dynamic";
+import { useEffect } from "react";
 export default function (){
     const router = useRouter()
     const {CustomerId} = router.query;
-    
+   
+   
     return (
      <>
     <div>
      <h1>Project Page</h1>
      { Db.map((userDet) => {
-        let urlLast = window.location.href()
-       let last =  Db.filter((x)=> x.name === urlLast)
-         console.log(urlLast)
-  return <h1> <Link href={`/costumers/${userDet.name}/${userDet.project}`}>{userDet.project}</Link></h1>
+  useEffect(()=>{
+    let urlLast = window.location.href.slice(-1)
+    let last=  Db.filter((x)=> x.id == urlLast)
+
+}, [])
+       
+  return <h1> <Link href={`/costumers/${userDet.name}/${userDet.project}`}>{last}</Link></h1>
  })}
+ 
     </div>
      </>
      
