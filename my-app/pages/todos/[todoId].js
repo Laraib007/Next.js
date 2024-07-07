4fcd23a22ae295ca54b7800c89948caa4e6fa5f5
Laraib007 <lrbkhan01@gmail.com>
2024-07-07 12:32:56 +0500

    commenting code

--- my-app/pages/todos/[todoId].js
+++ my-app/pages/todos/[todoId].js
@@ -1,33 +1,33 @@
-export default function (props){
-    const {todo} = props;
-    if(!todo){
-        return <div>Loading......</div>
-    }
-    return (
-        <div>{todo.title}</div>
-    )
-}
+// export default function (props){
+//     const {todo} = props;
+//     if(!todo){
+//         return <div>Loading......</div>
+//     }
+//     return (
+//         <div>{todo.title}</div>
+//     )
+// }
 
-export async function  getStaticProps ({params}) {
-    const {todoId} = params;
-    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
-    const data = await res.json()
-    return { 
-      props: {
-        todo: data
-    },revalidate: 10,
- }
-  }
+// export async function  getStaticProps ({params}) {
+//     const {todoId} = params;
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+//     const data = await res.json()
+//     return { 
+//       props: {
+//         todo: data
+//     },revalidate: 10,
+//  }
+//   }
 
-  export async function getStaticPaths ()  {
-    return {
-      paths: [
-        {
-          params: {
-            todoId: "1",
-          },
-        }, // See the "paths" section below
-      ],
-      fallback: "blocking", // false or "blocking"
-    }
-  }
+//   export async function getStaticPaths ()  {
+//     return {
+//       paths: [
+//         {
+//           params: {
+//             todoId: "1",
+//           },
+//         }, // See the "paths" section below
+//       ],
+//       fallback: "blocking", // false or "blocking"
+//     }
+//   }
